0955679a401c9890d9999993451b79f52319ae79
uuso <mymail.uuso@gmail.com>
2020-03-25 20:05:09 +0300

    some fillings

--- jlibrary/models.py
+++ jlibrary/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):    
+    full_name = models.TextField()
+    country = models.CharField(max_length=2)
+    birth_year = models.SmallIntegerField()
+    
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
