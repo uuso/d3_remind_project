c0a39edcd6e58465e0624edb9797202372d660e7
uuso <mymail.uuso@gmail.com>
2020-03-31 14:03:46 +0300

    first views

--- jlibrary/models.py
+++ jlibrary/models.py
@@ -12,3 +12,8 @@ class Book(models.Model):
     description = models.TextField()
     year_release = models.SmallIntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    copy_count = models.SmallIntegerField(default=1)
+    price = models.DecimalField(max_digits = 7, decimal_places=2)
+    
+    def __str__(self):
+        return self.title
--- jlibrary/views.py
+++ jlibrary/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def books_list(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books_count = Book.objects.count()
+    bib_data = {'title': 'мою библиотэку', 'count': books_count, }
+    return HttpResponse(template.render(bib_data))
